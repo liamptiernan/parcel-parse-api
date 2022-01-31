e85261be6966885423c6801f68d1fad0ded2bb6a
Liam Tiernan <liamptiernan@gmail.com>
2022-01-31 18:38:10 -0500

    Heroku Prep

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const createCSV = require('./parcel-scraper');
-const htmlUpdate = require('./page-call');
-const path = require('path');
 
 const app = express();
-const port = 5000;
+let port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = 5000;
+}
 
 app.use(express.json());
-
-async function getCSV(req, res) {
-  const filePath = await createCSV.getData(req);
-  res.sendFile(path.resolve(__dirname, filePath));
-}
 
 app.get('/api/csv', (req, res) => {
   console.log('request received')
@@ -28,8 +24,11 @@ app.post('/api/csv', (req, res) => {
 
 app.post('/api/html', (req, res) => {
   console.log(req.body);
-  htmlUpdate.getHtml(req.body.qty);
   res.send('OK')
+})
+
+app.get('/health', (req, res) => {
+  res.send('OK');
 })
 
 app.get('/', (req, res) => {
